5711324b9cd590b95f32347e01b2257a5497e961
smaine-coon <hikaedi18hei06@gmail.com>
2026-01-28 13:57:17 +0900

    MODIFY: Path changed

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
+$env:Path = ($env:Path -split ';' | Where-Object { $_ -ne "$HOME\AppData\Local\mise\shims" }) -join ';'
+$env:Path = "$HOME\AppData\Local\mise\shims;$env:Path"
 mise activate pwsh | Out-String | Invoke-Expression
 mise activate pwsh --shims | Out-String | Invoke-Expression
