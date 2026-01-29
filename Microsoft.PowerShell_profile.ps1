750199f29701e70a336be16e62afca6eebff9003
smaine-coon <hikaedi18hei06@gmail.com>
2026-01-29 10:15:20 +0900

    Refactor PowerShell profile for shim path management

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
-$env:Path = ($env:Path -split ';' | Where-Object { $_ -ne "$HOME\AppData\Local\mise\shims" }) -join ';'
-$env:Path = "$HOME\AppData\Local\mise\shims;$env:Path"
+$shimPath = "$HOME\AppData\Local\mise\shims"
+$paths = $env:Path -split ';' | Where-Object { $_ -ne $shimPath }
+# $paths = $env:Path -split ';' | Where-Object { $_ -notlike "*\shims" }
+$env:Path = "$shimPath;" + ($paths -join ';')
 mise activate pwsh | Out-String | Invoke-Expression
 mise activate pwsh --shims | Out-String | Invoke-Expression
